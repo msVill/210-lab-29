8a2a02911baa1541edd2ac54a2573bc243418306
msVill <msv.ltmc@gmail.com>
2026-04-15 21:07:47 -0700

    Added a few comments along with libraries

---
+++ main.cpp
@@ -0,0 +1,17 @@
+// Necessary libraries
+#include <iostream>
+#include <map>
+#include <array>
+#include <list>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+//Function to simulate city changes over time:
+
+//Loop through each time period:
+
+//Loop through each city:
+
+//
